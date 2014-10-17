9831d1efc01f67aa6768d6a0b01fffcd13a2378c
Levin120 <lvjiawendlut@gmail.com>
2014-10-17 10:40:05 +0800

    make codes shorter

--- Same_Tree/Solution.java
+++ Same_Tree/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
             return true;
-        else if ((p == null && q != null)||(p != null && q == null))
+        else if (p == null || q == null)
             return false;
         else {
             if(isSameTree(p.left, q.left) == true && isSameTree(p.right, q.right) == true && p.val == q.val) 
