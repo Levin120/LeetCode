22b118025c53359234d1e58d9bb227e354121fc6
Levin120 <lvjiawendlut@gmail.com>
2014-10-24 23:40:13 +0800

    modify the code

--- Remove_Duplicates_from_Sorted_Array/Solution.java
+++ Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
             return 0;
         int len = 1;
         for(int i = 1; i < A.length; i++) {
-            while(A[i] != A[i - 1]) {
+            if(A[i] != A[i - 1]) {
                 for(int j = i - 1; j > len - 1; j--) {
                     A[j] = A[i];
                 }
