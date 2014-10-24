382b512d7e9852eee7b7bf27c9e072019a125ec7
Levin120 <lvjiawendlut@gmail.com>
2014-10-24 23:51:16 +0800

    make the code shorter

--- Remove_Duplicates_from_Sorted_Array/Solution.java
+++ Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -1,15 +1,10 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
-        if(A.length == 0)
-            return 0;
-        int len = 1;
-        for(int i = 1; i < A.length; i++) {
-            if(A[i] != A[i - 1]) {
-                for(int j = i - 1; j > len - 1; j--) {
-                    A[j] = A[i];
-                }
-                len++;
-            }
+        int len = 0;
+        for(int i = 0; i < A.length; i++) {
+            if((i + 1) < A.length && (A[i] == A[i+1]))
+                continue;
+            A[len++] = A[i];
         }
         return len;
     }
