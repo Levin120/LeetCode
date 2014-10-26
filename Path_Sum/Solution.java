35f154567fa22e70f7517ef345618a7ad0df62fe
Levin120 <lvjiawendlut@gmail.com>
2014-10-26 22:51:18 +0800

    The solution of Path Sum.

---
+++ Path_Sum/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null)
+            return root.val == sum;
+        return (root.left != null && hasPathSum(root.left, sum - root.val))
+            || (root.right != null && hasPathSum(root.right, sum - root.val));
+    }
+}
