e935d3c958b17d06177922d8e79513d0beeca381
Levin120 <lvjiawendlut@gmail.com>
2014-10-30 22:42:48 +0800

    the solution of String to Integer atoi

---
+++ String_to_Integer_atoi/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int atoi(String str) {
+        if(str == null)
+            return 0;
+        str = str.trim();
+        if(str.length() == 0)
+            return 0;
+        boolean isNeg = false;
+        int i = 0;
+        if(str.charAt(0) == '-')
+        {
+            isNeg = true;
+            i++;
+        } 
+        else if(str.charAt(0) == '+')
+            i++;
+        int res = 0;
+        while(i < str.length()) {
+            if(str.charAt(i) < '0' || str.charAt(i) > '9')
+                break;
+            int digit = (int)(str.charAt(i) - '0');
+            if(isNeg && res > -((Integer.MIN_VALUE + digit) / 10))
+                return Integer.MIN_VALUE;
+            else if(!isNeg && res > (Integer.MAX_VALUE - digit) / 10)
+                return Integer.MAX_VALUE;
+            res = res * 10 + digit;
+            i++;
+        }
+        return isNeg?-res:res;
+    }
+}
